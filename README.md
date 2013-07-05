4d885f347547b20e28fdfabbf0eb3ac8eb6d237e
Juga Paazmaya <paazmaya@yahoo.com>
2013-07-05 16:01:31 +0300

    This is where things start to get interesting...

---
+++ README.md
@@ -0,0 +1,52 @@
+# grunt-trimtrailingspaces
+
+Trim trailing spaces from each line of the selected files
+
+## Getting Started
+
+Add this to your project's `Gruntfile.js` gruntfile:
+```js
+grunt.loadNpmTasks('grunt-trimtrailingspaces');
+```
+
+Then add "grunt-trimtrailingspaces" to your package.json dependencies. This can be done with:
+```js
+npm install grunt-trimtrailingspaces --save-dev
+```
+Or by manually editing the "package.json" by adding the following line inside `devDependencies` object:
+```js
+  "grunt-trimtrailingspaces": "~0.1.0"
+```
+
+Later on it would be possible to install the plugin with the command `npm install`
+
+It can be updated with the command `npm update`, in case there is a newer version in the NPM repository.
+
+The name to use in your own task definitions is `trimtrailingspaces`.
+
+## Documentation
+
+Add an entry to your "Gruntfile.js", within the `initConfig` object, which will define the 
+files of which will the trailing spaces to be removed.
+
+```js
+  ...
+
+  trimtrailingspaces: {
+    extensions: ['js', 'css', 'html'],
+    directories: ['.', 'templates', 'public_html/js', 'public_html/css'],
+    encoding: 'utf8'
+  }
+
+  ...
+```
+
+To run it:
+
+```js
+grunt trimtrailingspaces
+```
+
+## License
+Copyright (c) 2013 Juga Paazmaya <olavic@gmail.com>
+Licensed under the MIT license.
---
+++ tasks/trimtrailingspaces.js
@@ -0,0 +1,53 @@
+/*
+ * grunt-trimtrailingspaces
+ * https://github.com/paazmaya/grunt-trimtrailingspaces
+ *
+ * Copyright (c) 2013 Juga Paazmaya <olavic@gmail.com>
+ * Licensed under the MIT license.
+ */
+
+module.exports = function(grunt) {
+  "use strict";
+  
+  grunt.registerTask('trimtrailingspaces', function() {
+    var S = require('string'),
+      fs = require('fs'),
+      conf = grunt.config('trimtrailingspaces');
+
+    if (!conf.directories || conf.directories.length === 0) {
+      return ;
+    }
+
+    conf.directories.forEach(function (directory) {
+      grunt.verbose.writeln( 'Processing directory: ' + directory );
+      var dir = fs.readdirSync(directory);
+      dir.forEach(function (filename) {
+
+        var ext = filename.split('.').pop();
+        var path = directory + '/' + filename;
+        var st = fs.statSync(path);
+
+        if (conf.extensions.indexOf(ext) !== -1 && st.isFile()) {
+          grunt.verbose.writeln( 'Processing path: ' + path );
+
+          var content = fs.readFileSync(
+            path,
+            {encoding: conf.encoding}
+          );
+
+          var trimmed = [];
+          content.split("\n").forEach(function (line) {
+            trimmed.push(S(line).trimRight().s);
+          });
+
+          fs.writeFileSync(
+            path,
+            trimmed.join("\n"),
+            {encoding: conf.encoding}
+          );
+        }
+      });
+    });
+  });
+
+};

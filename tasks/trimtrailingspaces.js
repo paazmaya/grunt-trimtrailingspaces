096d12f726a68d4277e427f06a77c6c9f8276891
Michael Cebrian <michael.cebrian@thomsonreuters.com>
2015-08-05 11:36:41 -0400

    Update Messaging

--- tasks/trimtrailingspaces.js
+++ tasks/trimtrailingspaces.js
@@ -20,14 +20,17 @@ module.exports = function trimtrailingspaces(grunt) {
     var fsOptions = { // Filesystem access options
       encoding: options.encoding
     };
+
     var changedFileCount = 0;
+    var fileCount = 0;
+    var successCount = 0;
 
     this.files.forEach(function filesEach(file) {
 
       // Iterate src as that is where the actual files are
       file.src.forEach(function srcEach(src) {
         grunt.verbose.writeln('Processing file: ' + src);
-
+        fileCount++;
         var content = grunt.file.read(src, fsOptions);
         var destination = src;
         // dest might be undefined, thus use same directory as src
@@ -41,21 +44,25 @@ module.exports = function trimtrailingspaces(grunt) {
         var trimmed = content.replace(/[ \f\t\v]*$/gm, '');
 
         if (content !== trimmed) {
-          grunt.verbose.writeln('Needed trimming, file: ' + src);
-          ++changedFileCount;
+          grunt.log.ok('Needed trimming, file: ' + src);
           grunt.file.write(destination, trimmed, fsOptions);
+          changedFileCount++;
         }
         else if (src !== destination) {
           // Copy to destination if not in place modification
           grunt.file.copy(src, destination, fsOptions);
         }
-
-
       });
     });
 
-    if (changedFileCount > 0 && options.failIfTrimmed) {
-      grunt.fail.warn(changedFileCount + ' files had whitespace trimmed, and the failIfTrimmed option is set to true.', 6);
+    successCount = fileCount - changedFileCount;
+    grunt.log.ok(successCount + ' file' + (successCount === 1 ? '' : 's') + ' free of trailing whitespace.');
+
+    if (changedFileCount > 0) {
+      grunt.log.ok(changedFileCount + ' file' + (changedFileCount === 1 ? '' : 's') + ' had whitespace trimmed.');
+      if (options.failIfTrimmed) {
+        grunt.fail.warn(options.failMsg || 'The failIfTrimmed option is set to true.', 6);
+      }
     }
 
     return taskSucceeded;
